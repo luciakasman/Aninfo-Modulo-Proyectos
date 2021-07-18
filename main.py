8025cef9cb20b53155d544ab21b934f9ac9ea43b
Lucia Kasman <lkasman@medallia.com>
2021-07-17 23:07:22 -0300

    arreglar error CORS

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.routers import task_controller, project_controller
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5000",
+]
+
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=origins)
+]
 
 app = FastAPI()
 
