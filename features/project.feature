86e166bb7d5b747d87cd631553c8036943a07957
AgusVare <agussb.varela@gmail.com>
2021-07-24 16:51:01 -0300

    tests

--- features/project.feature
+++ features/project.feature
@@ -15,18 +15,12 @@ Scenario: 3- Camino : Como usuario, quiero poder obtener los datos de un proyect
   When  solicito la información con el identificador
   Then el sistema muestra el proyecto con los datos: nombre, líder de proyecto, personas asignadas que elegí, fecha de inicio, fecha límite de inicio, fecha fin y sus tareas asociadas.
 
-#pendientes
 Scenario: 4- Camino B: Como usuario, quiero poder modificar proyectos para actualizar la información. - update
-  Given que necesito actualizar la información de un proyecto
-  When modifico un proyecto
+  Given que necesito actualizar la información de un proyecto existente
+  When modifico el proyecto
   Then el sistema guarda el proyecto con los campos que le modifiqué, que pueden ser: el nombre, líder de proyecto, personas asignadas, fecha de inicio, fecha límite de inicio, fecha fin y/o tareas vinculadas al proyecto.
 
 Scenario: Como usuario, quiero poder borrar un proyecto para evitar tener proyectos en desuso.
     Given que quiero tener actualizados los proyectos
     When un proyecto está en desuso y lo elimino
     Then el sistema lo borra y no muestra más su información
-
-  Scenario: Agrego proyecto
-  Given que aguregué un proyecto
-  When consulto los proyectos
-  Then me muestra el nuevo proyecto
--- features/steps/project.py
+++ features/steps/project.py
@@ -6,6 +6,11 @@ empty = b'[]'
 
 body2 = """{"nombre": "Catalina", "lider_de_equipo": {"resourceID": 0, "name": "Romina","surname": "Gomez"},"personas_asignadas": [{"resourceID": 0,"name": "Tomas","surname": "Gomez"}],"fecha_inicio": "21/06/2021", "fecha_limite_inicio": "21/07/2021", "fecha_estimada_fin": "21/06/2021"}"""
 body3 = """{"nombre": "Franco", "lider_de_equipo": {"resourceID": 10, "name": "Romina","surname": "Gomez"},"personas_asignadas": [{"resourceID": 0,"name": "Tomas","surname": "Gomez"}],"fecha_inicio": "21/06/2021", "fecha_limite_inicio": "21/07/2021", "fecha_estimada_fin": "21/06/2021"}"""
+body4 = """{"nombre": "Tomas", "lider_de_equipo": {"resourceID": 10, "name": "Romina","surname": "Gomez"},"personas_asignadas": [{"resourceID": 0,"name": "Tomas","surname": "Gomez"}],"fecha_inicio": "21/06/2021", "fecha_limite_inicio": "21/07/2021", "fecha_estimada_fin": "21/06/2021"}"""
+body4Edited = """{"nombre": "Lautaro", "lider_de_equipo": {"resourceID": 10, "name": "Romina","surname": "Gomez"},"personas_asignadas": [{"resourceID": 0,"name": "Tomas","surname": "Gomez"}],"fecha_inicio": "21/06/2021", "fecha_limite_inicio": "21/07/2021", "fecha_estimada_fin": "21/06/2021"}"""
+body5 = """{"nombre": "Julieta", "lider_de_equipo": {"resourceID": 10, "name": "Romina","surname": "Gomez"},"personas_asignadas": [{"resourceID": 0,"name": "Tomas","surname": "Gomez"}],"fecha_inicio": "21/06/2021", "fecha_limite_inicio": "21/07/2021", "fecha_estimada_fin": "21/06/2021"}"""
+completeApi = 'http://127.0.0.1:8000/projects/'
+
 
 @given('que no hay proyectos existentes')
 def no_projects(context):
@@ -14,35 +19,65 @@ def no_projects(context):
 
 @given('que necesito crear proyectos al sistema')
 def create_projects(context):
-    global apiComplete
-    apiComplete = 'http://127.0.0.1:8000/projects/'
+    global completeApi
+    completeApi = 'http://127.0.0.1:8000/projects/'
 
 @given('soy un usuario y quiero visualizar la información de un proyecto')
 def view_information(context):
-    global apiComplete, id
-    apiComplete = 'http://127.0.0.1:8000/projects/'
+    global id
 
     bodyJson = json.loads(body3)
-    id = requests.post(apiComplete, json=bodyJson).content.decode()
+    id = requests.post(completeApi, json=bodyJson).content.decode()
+
+
+@given('que necesito actualizar la información de un proyecto existente')
+def update_information(context):
+    global id
+
+    bodyJson = json.loads(body4)
+    id = requests.post(completeApi, json=bodyJson).content.decode()
+
+@given('que quiero tener actualizados los proyectos')
+def update_information(context):
+    global id
+
+    bodyJson = json.loads(body5)
+    id = requests.post(completeApi, json=bodyJson).content.decode()
+
 
 @when('consulto los proyectos')
 def get_all_projects(context):
     global result
-    apiComplete = apiUrl+"projects/"
-    result = requests.get(apiComplete)
+    completeApi = apiUrl+"projects/"
+    result = requests.get(completeApi)
 
 @when('agrego un proyecto')
 def add_project(context):
         global result, bodyJson
         bodyJson = json.loads(body2)
-        requests.post(apiComplete, json = bodyJson)
+        requests.post(completeApi, json = bodyJson)
 
 
 @when('solicito la información con el identificador')
 def project_by_id(context):
         global result
-        api = apiComplete + id
+        api = completeApi + id
         result = requests.get(api)
+
+@when('modifico el proyecto')
+def modify_project(context):
+        global result, url
+        url = completeApi + id
+        bodyJson = json.loads(body4Edited)
+
+        result = requests.put(url, json = bodyJson)
+
+@when('un proyecto está en desuso y lo elimino')
+def modify_project(context):
+        global result, url
+        url = completeApi + id
+
+        result = requests.delete(url)
 
 @then('no me muestra ningún proyecto')
 def not_found_projects(context):
@@ -50,57 +85,26 @@ def not_found_projects(context):
 
 @then('el sistema carga el proyecto con el nombre, líder de proyecto, personas asignadas que elegí, fecha de inicio, fecha límite de inicio, fecha fin.')
 def all_projects(context):
-    result = requests.get(apiComplete)
+    result = requests.get(completeApi)
     assert compareJsons(result.content, body2)
 
 @then( 'el sistema muestra el proyecto con los datos: nombre, líder de proyecto, personas asignadas que elegí, fecha de inicio, fecha límite de inicio, fecha fin y sus tareas asociadas.')
 def all_projects(context):
     assert compareJsons(result.content, body3)
 
+@then( 'el sistema guarda el proyecto con los campos que le modifiqué, que pueden ser: el nombre, líder de proyecto, personas asignadas, fecha de inicio, fecha límite de inicio, fecha fin y/o tareas vinculadas al proyecto.')
+def all_projects(context):
+    result = requests.get(url)
+    assert compareJsons(result.content, body4Edited)
+    assert not compareJsons(result.content, body4)
+
+@then( 'el sistema lo borra y no muestra más su información')
+def all_projects(context):
+    result = requests.get(url)
+    assert not compareJsons(result.content, body5)
 
 def compareJsons(result, body):
     resultToCompare = result.decode()
     bodyToCompare = body.replace(": ", ":").replace(", ", ",")[0:-1]
     return bodyToCompare in resultToCompare
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@given('que aguregué un proyecto')
-def no_projects(context):
-    global apiUrl, body
-    apiUrl = "http://127.0.0.1:8000/projects/"
-    body = {
-  "nombre": "Camila",
-  "lider_de_equipo": {
-    "legajo": 1,
-    "Nombre": "Carolina",
-    "Apellido": "Sanchez"
-  },
-  "personas_asignadas": [
-    {
-      "legajo": 2,
-      "Nombre": "Carla",
-      "Apellido": "Gomez"
-    }
-  ],
-  "fecha": "01/01/2021"
-}
-    res = requests.post(apiUrl, body)
-
-@then('me muestra el nuevo proyecto')
-def step_impl(context):
-    result = requests.get(apiComplete)
-    assert True is True
